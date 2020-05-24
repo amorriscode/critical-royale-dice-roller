c4da1bd378f3039c59cdcba5b92ab2f8fc9c7966
Anthony Morris <anthony@amorrissound.com>
2020-05-23 17:39:55 -0700

    Add open graph image

--- pages/_app.js
+++ pages/_app.js
@@ -12,17 +12,17 @@ class CriticalRoyaleApp extends App {
         <DefaultSeo
           openGraph={{
             title: 'Critical Royale Dice Roller',
-            description: 'Choose your weapon. Roll some dice for your next great adventure!',
+            description: 'Choose your weapon. Roll some dice on your next adventure!',
             type: 'website',
-            url: 'https://dice.criticalroyale.com',
+            url: 'https://roll.criticalroyale.com',
             site_name: 'Critical Royale Dice Roller',
             images: [
-              // {
-              //   url: `https://dummyimage.com/800x600/${getRandomHexColor()}/fff.png&text=id8`,
-              //   width: 800,
-              //   height: 600,
-              //   alt: 'id8 is like bingo... a riot!',
-              // },
+              {
+                url: `https://roll.criticalroyale.com/og.png`,
+                width: 800,
+                height: 600,
+                alt: 'Only the dice can decide your fate!',
+              },
             ],
           }}
           twitter={{
